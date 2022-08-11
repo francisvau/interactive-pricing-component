34e262d1a052eb480ba88d9428d4a1284c0c8a73
francisvau <vauterinf@gmail.com>
2022-08-11 11:28:38 +0200

    slider with useState

--- src/components/MoneySlider.js
+++ src/components/MoneySlider.js
@@ -1,17 +1,22 @@
 
+import { useState } from "react"
+
 const MoneySlider = () => {
+
+    const [amount, setAmount] = useState(50)
+
   return (
     <div className="payment--money-slider">
         <div className="money-slider--text">
             <p>100K PAGEVIEWS</p>
             <span>
-                <h1>$16.00</h1>
+                <h1>${amount}</h1>
                 <p>/month</p>
             </span>
         </div>
 
         <div className="money-slider--slider">
-            <input type="range" min="1" max="100" />
+            <input type="range" min="1" max="100" value={amount} onChange={(e) => setAmount(e.target.value)}/>
         </div>
     </div>
   )
