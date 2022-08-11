3f10bdec522b2d21770c846c535a6f726bde8246
francisvau <vauterinf@gmail.com>
2022-08-11 11:55:55 +0200

    billing with minimal css

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -1,7 +1,12 @@
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <div className="payment--billing">
+      <p className="billing--option">Monthly Billing</p>
+      <label className="billing--switch"><input type="checkbox" /><div></div></label>
+      <p className="billing--option">Yearly Billing</p>
+      <p className="billing--discound">25% discount</p>
+    </div>
   )
 }
 
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,10 +1,12 @@
 
+import Billing from "./Billing"
 import MoneySlider from "./MoneySlider"
 
 const Payment = () => {
   return (
     <div className="pricecard--payment">
       <MoneySlider />
+      <Billing />
     </div>
   )
 }
